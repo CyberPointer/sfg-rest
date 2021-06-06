f9fb8b2a030fbe9010ac430589837d18c8d8c118
bob <test@example.com>
2021-06-06 18:02:36 +0200

    Implemented VendorAPI with TDD

---
+++ target/generated-sources/annotations/guru/springfamework/api/v1/mapper/VendorMapperImpl.java
@@ -0,0 +1,43 @@
+package guru.springfamework.api.v1.mapper;
+
+import guru.springfamework.api.v1.model.VendorDTO;
+import guru.springfamework.domain.Vendor;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-06-06T17:47:48+0200",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.8 (Amazon.com Inc.)"
+)
+@Component
+public class VendorMapperImpl implements VendorMapper {
+
+    @Override
+    public VendorDTO vendorToVendorDTO(Vendor vendor) {
+        if ( vendor == null ) {
+            return null;
+        }
+
+        VendorDTO vendorDTO = new VendorDTO();
+
+        vendorDTO.setId( vendor.getId() );
+        vendorDTO.setName( vendor.getName() );
+
+        return vendorDTO;
+    }
+
+    @Override
+    public Vendor vendorDTOtoVendor(VendorDTO vendorDTO) {
+        if ( vendorDTO == null ) {
+            return null;
+        }
+
+        Vendor vendor = new Vendor();
+
+        vendor.setId( vendorDTO.getId() );
+        vendor.setName( vendorDTO.getName() );
+
+        return vendor;
+    }
+}
